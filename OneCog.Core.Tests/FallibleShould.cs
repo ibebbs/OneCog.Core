811c5d3eba77906702d0e4e1b7951ed568746024
ibebbs <github@bebbs.co.uk>
2015-02-26 08:34:00 +0000

    Initial commit
    Core classes for OneCog projects

---
+++ OneCog.Core.Tests/FallibleShould.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneCog.Core.Tests
+{
+    [TestFixture]
+    public class FallibleShould
+    {
+        [Test]
+        public void ReturnASuccessfulResultWhenBuiltFromValue()
+        {
+            IFallible<int> actual = Fallible.FromValue<int>(314);
+
+            Assert.That(actual.IsValue, Is.True);
+            Assert.That(actual.IsError, Is.False);
+        }
+
+        [Test]
+        public void ReturnHoldValueAWhenBuiltFromValue()
+        {
+            IFallible<int> actual = Fallible.FromValue<int>(314);
+
+            Assert.That(actual.Value, Is.EqualTo(314));
+            Assert.That(actual.Error, Is.Null);
+        }
+
+        [Test]
+        public void ReturnAErrorResultWhenBuiltFromError()
+        {
+            IFallible<int> actual = Fallible.FromError<int>(new ArgumentException());
+
+            Assert.That(actual.IsError, Is.True);
+            Assert.That(actual.IsValue, Is.False);
+        }
+
+        [Test]
+        public void ReturnHoldErrorAWhenBuiltFromError()
+        {
+            IFallible<int> actual = Fallible.FromError<int>(new ArgumentException());
+
+            Assert.That(actual.Error, Is.InstanceOf<ArgumentException>());
+            Assert.That(actual.Value, Is.EqualTo(default(int)));
+        }
+    }
+}
---
+++ OneCog.Core/Fallible.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneCog.Core
+{
+    public static class Fallible
+    {
+        public static IFallible<T> FromValue<T>(T value)
+        {
+            return new Fallible<T>(value);
+        }
+
+        public static IFallible<T> FromError<T>(Exception error)
+        {
+            return new Fallible<T>(error);
+        }
+
+        public static IFallible<T> From<T>(Func<T> func)
+        {
+            try
+            {
+                return FromValue(func());
+            }
+            catch (Exception error)
+            {
+                return FromError<T>(error);
+            }
+        }
+
+        public static async Task<IFallible<T>> FromAsync<T>(Func<Task<T>> func)
+        {
+            try
+            {
+                return FromValue(await func());
+            }
+            catch (Exception error)
+            {
+                return FromError<T>(error);
+            }
+        }
+    }
+
+    public interface IFallible<T>
+    {
+        Exception Error { get; }
+        T Value { get; }
+        bool IsValue { get; }
+        bool IsError { get; }
+    }
+
+    public class Fallible<T> : IFallible<T>
+    {
+        public Fallible(T value)
+        {
+            Value = value;
+            IsValue = true;
+        }
+
+        public Fallible(Exception error)
+        {
+            Error = error;
+            IsError = true;
+        }
+
+        public Exception Error { get; set; }
+        public T Value { get; set; }
+        public bool IsValue { get; set; }
+        public bool IsError { get; set; }
+    }
+}
